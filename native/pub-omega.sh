16e9595ec335fa62336d3df34cb74632e91b1e08
uniseraph <zhengtao.wuzt@gmail.com>
2017-07-04 11:10:02 +0800

    fix

--- native/pub-omega.sh
+++ native/pub-omega.sh
@@ -6,16 +6,28 @@ cd `dirname $0`
 SERVICE_NAME=$1
 VERSION=$2
 
-pscp -h hosts/$SERVICE_NAME  -l  admin  ../../lib/${SERVICE_NAME}-${VERSION}.jar /home/admin/omega-framework/lib/
 
-CMD=" curl --connect-timeout 2 -fsSL -X POST http://localhost:8080/shutdown ;  \
-      sleep 5 &&  \
-      java -Djava.security.egd=file:/dev/./urandom -jar /home/admin/omega-framework/lib/${SERVICE_NAME}-${VERSION}.jar \
+CURRENT_USER=`whoami`
+cd
+HOME_DIR=`pwd`
+cd $curr_path
+
+
+EUREKA1=`cat hosts/eureak1`
+EUREKA2=`cat hosts/eureak2`
+EUREKA3=`cat hosts/eureak3`
+
+
+pscp -h hosts/$SERVICE_NAME  -l  ${CURRENT_USER}  ~/omega-framework-assembly-0.1/lib/${SERVICE_NAME}-${VERSION}.jar ${HOME_DIR}/omega-framework/lib/
+
+CMD=" curl --connect-timeout 2 -fsSL -X POST http://127.0.0.1:8080/shutdown ;  \
+      sleep 5 &&  cd ${HOME_DIR}/omega-framework \
+      java -Djava.security.egd=file:/dev/./urandom -jar lib/${SERVICE_NAME}-${VERSION}.jar \
                                --spring.cloud.config.discovery.enabled=true  \
                                --spring.cloud.config.profile=test  \
                                --spring.cloud.config.label=master  \
-                               --eureka.client.serviceUrl.defaultZone=http://eureka1:8080/eureka/,http://eureka2:8080/eureka,http://eureka3:8080/eureka "
+                               --eureka.client.serviceUrl.defaultZone=http://${EUREKA1}:8080/eureka/,http://${EUREKA2}:8080/eureka,http://${EUREKA3}:8080/eureka "
 
-pssh -h hosts/$SERVICE_NAME -l admin -i  $CMD
+pssh -h hosts/$SERVICE_NAME -l ${CURRENT_USER} -i  $CMD
 
 cd $curr_path
